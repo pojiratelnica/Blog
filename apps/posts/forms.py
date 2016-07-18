96fd0a4262082e87e9d6a94c1768380532dc1e8b
Meerim <pojiratelnica@gmail.com>
2016-07-18 20:29:56 +0600

    clean forms:title, slug, image sizes

--- apps/posts/forms.py
+++ apps/posts/forms.py
@@ -3,19 +3,20 @@ from django import forms
 from models import Posts
 # from django.contrib.auth.models import User
 from redactor.widgets import RedactorEditor
+# from django.core import validators
+from PIL import Image
 
 
 class PostsForm(forms.ModelForm):
     class Meta:
         model = Posts
-        # Posts.title = forms.CharField(label='Your name')
+        fields = ('title', 'slug', 'author', 'category', 'display', 'image', 'text')
         widgets = {
            'text': forms.Textarea(attrs={
                 'class': 'form-control',
+                'cols': '3',
+                'rows': '4',
             }),
-           # 'likes': forms.IntegerField(attrs={
-           #      'class': 'fofm-control',
-           #  })
            'title': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': u'Название',
@@ -30,20 +31,38 @@ class PostsForm(forms.ModelForm):
            'category': forms.Select(attrs={
                 'class': 'form-control',
             }),
-           'image': forms.FileInput(attrs={
-                'class': 'btn btn-primary',
-            }),
-           # 'display': forms.CheckboxInput(attrs={
-           #      'class': 'form-control',
-           #  }),
-           'likes': forms.TextInput(attrs={
-                'class': 'form-control',
-            }),
-           'dislikes': forms.TextInput(attrs={
-                'class': 'form-control',
-            }),
         }
         exclude = ()
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) < 5:
+            raise forms.ValidationError(u'Давай больше пиши')
+        return title
+
+    def clean_slug(self):
+        slug = self.cleaned_data['slug']
+        if Posts.objects.filter(slug=slug).exists():
+            raise forms.ValidationError(u'Такой уже есть')
+        return slug
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image', False)
+        img = Image.open(image)
+        width, height = img.size
+        if image:
+            if height < 400 and width < 400:
+                raise forms.ValidationError(u'Слишком маленькое изображение < 400px*400px')
+            elif image._size < 1*1024*1024:
+                raise forms.ValidationError(u'Слишком маленькое изображение < 1mb')
+            elif image._size > 4*1024*1024:
+                raise forms.ValidationError(u'Труба тяжелое изображение > 4mb')
+            return image
+        else:
+            raise forms.ValidationError(u'Не могу загрузить изображение')
+
+    def clean(self):
+        pass
 
 
 class PostsAdminForm(forms.ModelForm):
